ad9338a91c609256afbf2217d3f8b6476afc21b6
ErickMGG06 <erickmaster571@gmail.com>
2026-09-20 18:54:36 -0500

    Algunos datos agregados

--- Sem02-2.py
+++ Sem02-2.py
@@ -7,3 +7,13 @@ print(f"En 10 años tendra {edad+10}")
 
 edad = int(input("Ingrese la edad:\n"))
 print(f"En 10 años tendra {edad+10}")
+
+#Metodos o Funciones 
+def Imprimir(texto):
+    print(texto)
+
+def Suma10edad(edad):
+    return edad + 10
+
+Imprimir("Hola")
+print(Suma10edad(20))
